af6c2d0c84cb144a1a0b7e85e3d8d95ba5db5d6a
Kemunto-wendy <joy.kemunto@student.moringaschool.com>
2023-04-12 19:43:34 +0300

    fixed the blocker

--- medimind-frontend/src/App.js
+++ medimind-frontend/src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header/header';
-import Login from './components/Login/login';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 const App = ( ) => {
   return (
+
     <div className="App">
-  {/* Header /> */}
-<Login />
+
+        <Header />
+
               </div>
 
   );
